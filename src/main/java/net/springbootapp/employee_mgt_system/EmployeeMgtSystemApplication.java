51c81f821590f67559acca3749b5569da071a2b6
zehan12 <zehan9211@gmail.com>
2024-07-10 22:32:02 +0530

    create spring boot app and add config

---
+++ src/main/java/net/springbootapp/employee_mgt_system/EmployeeMgtSystemApplication.java
@@ -0,0 +1,13 @@
+package net.springbootapp.employee_mgt_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/springbootapp/employee_mgt_system/EmployeeMgtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springbootapp.employee_mgt_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

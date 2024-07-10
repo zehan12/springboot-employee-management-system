2e3fe1e2494a0d81e42d441322ac51e2e093b1a0
zehan12 <zehan9211@gmail.com>
2024-07-10 22:44:36 +0530

    add user schema

---
+++ src/main/java/net/springbootapp/employee_mgt_system/entity/Employee.java
@@ -0,0 +1,34 @@
+package net.springbootapp.employee_mgt_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+}
